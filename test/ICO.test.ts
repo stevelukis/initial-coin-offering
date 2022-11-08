4a86987eb6cd23a3b5a212a0572e9164d8ff23b4
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-08 10:52:32 +0800

    Add tests for buy function

--- test/ICO.test.ts
+++ test/ICO.test.ts
@@ -7,13 +7,13 @@ import { expect } from "chai";
 describe("ICO", function () {
     async function deployICOFixture() {
         const ICO: ContractFactory = await ethers.getContractFactory("ICO");
-        const [owner, investor]: SignerWithAddress[] = await ethers.getSigners();
+        const [owner, investor, nonInvestor]: SignerWithAddress[] = await ethers.getSigners();
 
         // Deploy the contract
         const ico: Contract = await ICO.deploy("Token", "TOKEN", 100);
         await ico.deployed();
 
-        return { ico, owner, investor };
+        return { ico, owner, investor, nonInvestor };
     }
 
     describe("Start", function () {
@@ -36,30 +36,30 @@ describe("ICO", function () {
             const { ico } = await loadFixture(deployICOFixture);
 
             await expect(ico.start(0, 100, 100, 1, 100))
-                .to.be.revertedWith("Duration should be > 0.");
+                .to.be.revertedWith("Duration should be > 0");
         });
 
         it("Should be reverted if _availableToken is not valid", async function () {
             const { ico } = await loadFixture(deployICOFixture);
 
             await expect(ico.start(100, 100, 0, 1, 100))
-                .to.be.revertedWith("_availableTokens is not valid.");
+                .to.be.revertedWith("_availableTokens is not valid");
             await expect(ico.start(100, 100, 100000, 1, 100))
-                .to.be.revertedWith("_availableTokens is not valid.");
+                .to.be.revertedWith("_availableTokens is not valid");
         });
 
         it("Should be reverted if _minPurchase is not valid", async function () {
             const { ico } = await loadFixture(deployICOFixture);
             await expect(ico.start(100, 100, 100, 0, 100))
-                .to.be.revertedWith("minPurchase is not valid.");
+                .to.be.revertedWith("minPurchase is not valid");
         });
 
         it("Should be reverted if _maxPurchase is not valid", async function () {
             const { ico } = await loadFixture(deployICOFixture);
             await expect(ico.start(100, 100, 100, 1, 0))
-                .to.be.revertedWith("maxPurchase is not valid.");
+                .to.be.revertedWith("maxPurchase is not valid");
             await expect(ico.start(100, 100, 100, 1, 101))
-                .to.be.revertedWith("maxPurchase is not valid.");
+                .to.be.revertedWith("maxPurchase is not valid");
         });
     });
 
@@ -82,4 +82,70 @@ describe("ICO", function () {
         });
     });
 
+    describe("Buy", function () {
+        const PRICE = 5;
+        const AVAILABLE_TOKENS = 100;
+        const MIN_PURCHASE = 5;
+        const MAX_PURCHASE = 100;
+
+        async function preBuyFixture() {
+            const deployedUtils = await deployICOFixture();
+            const { ico, investor } = deployedUtils;
+
+            await ico.start(100, PRICE, AVAILABLE_TOKENS, MIN_PURCHASE, MAX_PURCHASE);
+            await ico.whitelist(investor.address);
+
+            return deployedUtils
+        }
+
+        it("Should run successfully", async function () {
+            const { ico, investor } = await loadFixture(preBuyFixture);
+
+            const quantity = MIN_PURCHASE;
+            const value = quantity * PRICE;
+            await ico.connect(investor).buy({ value });
+
+            const sale = await ico.sales(0);
+            expect(sale.investor).to.be.equal(investor.address);
+            expect(sale.quantity).to.be.equal(quantity);
+        });
+
+        it("Should be reverted if not called by investors", async function () {
+            const { ico, nonInvestor } = await loadFixture(preBuyFixture);
+            await expect(ico.connect(nonInvestor).buy({ value: PRICE }))
+                .to.be.revertedWith("Only investors can call this function");
+        });
+
+        it("Should be reverted if not paying the multiple of the price", async function () {
+            const { ico, investor } = await loadFixture(preBuyFixture);
+            await expect(ico.connect(investor).buy({ value: PRICE + 1 }))
+                .to.be.revertedWith("Ethers sent should be a multiple of price");
+        });
+
+        it("Should be reverted if not buying between the minimum and maximum purchase", async function () {
+            const { ico, investor } = await loadFixture(preBuyFixture);
+            await expect(ico.connect(investor).buy({ value: (MIN_PURCHASE - 1) * PRICE }))
+                .to.be.revertedWith("Number of tokens purchased should be between minPurchase and maxPurchase");
+            await expect(ico.connect(investor).buy({ value: (MAX_PURCHASE + 1) * PRICE }))
+                .to.be.revertedWith("Number of tokens purchased should be between minPurchase and maxPurchase");
+        });
+
+        it("Should be reverted if quantity is more that available tokens", async function () {
+            const { ico, investor } = await loadFixture(preBuyFixture);
+
+            // Purchase almost all the available tokens first
+            await ico.connect(investor).buy({ value: (MAX_PURCHASE - 1) * PRICE });
+
+            await expect(ico.connect(investor).buy({value: MIN_PURCHASE * PRICE}))
+                .to.be.revertedWith("Not enough tokens left for sale");
+        });
+
+        it("Should be reverted if ICO is not activated", async function () {
+            const { ico, investor } = await loadFixture(deployICOFixture);
+            await ico.whitelist(investor.address);
+
+            await expect(ico.connect(investor).buy({ value: PRICE }))
+                .to.be.revertedWith("ICO should be active");
+        });
+    })
 })
